19ba3bce9ea86390ec77f5d5831639155472473a
zhangmeteor <438114643@qq.com>
2013-07-13 02:13:52 +0800

    coconut validate

--- app/models/seat.rb
+++ app/models/seat.rb
@@ -1,2 +1,14 @@
 class Seat < ActiveRecord::Base
+  belongs_to :flight
+  validate :baggage_does_not_exceed_baggage_capacity,:seats_available
+  def baggage_does_not_exceed_baggage_capacity
+    if baggage > flight.baggage_allowance
+      errors.add(:baggage,"You have too much baggage")
+    end
+  end
+  def seats_available
+    if flight.seats.size >= flight.capacity
+     errors.add(:flight_id,"The flight is fully booked")
+    end
+  end
 end
